7e08a4773b421826f9c697650028e3d440b34e97
Reinier van der Leer <github@pwuts.nl>
2022-08-14 22:51:19 +0200

    add firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:workmanager/workmanager.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import '/utils/notifications.dart' as notifications;
 import '/utils/etv_style.dart';
+import 'firebase_options.dart';
 import '/background.dart';
 import '/router.gr.dart';
 
@@ -28,6 +30,8 @@ void main() async {
 
   final appRouter = AppRouter();
   await notifications.initPlugin(appRouter);
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(EtvApp(router: appRouter));
 }
