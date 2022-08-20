87db99b5f464ca59a65116847aae0d627f829bbe
Tom <T.O.Geerling@student.tudelft.nl>
2022-08-20 18:42:01 +0200

    progress on firebase messaging

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:etv_app/push_notifications.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -7,11 +8,19 @@ import '/utils/etv_style.dart';
 import 'firebase_options.dart';
 import '/router.gr.dart';
 
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('user');
 
   await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   await PushNotificationsManager().init();
 
   runApp(EtvApp());
--- lib/push_notifications.dart
+++ lib/push_notifications.dart
@@ -1,12 +1,12 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 class PushNotificationsManager {
-
   PushNotificationsManager._();
 
   factory PushNotificationsManager() => _instance;
 
-  static final PushNotificationsManager _instance = PushNotificationsManager._();
+  static final PushNotificationsManager _instance =
+      PushNotificationsManager._();
 
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
   bool _initialized = false;
@@ -14,7 +14,29 @@ class PushNotificationsManager {
   Future<void> init() async {
     if (!_initialized) {
       // For iOS request permission first.
-      _firebaseMessaging.requestPermission();
+
+      NotificationSettings settings =
+          await _firebaseMessaging.requestPermission(
+        alert: true,
+        announcement: true,
+        badge: true,
+        carPlay: false,
+        criticalAlert: false,
+        provisional: false,
+        sound: true,
+      );
+      print('User granted permission: ${settings.authorizationStatus}');
+
+
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+        print('Got a message whilst in the foreground!');
+        print('Message data: ${message.data}');
+
+        if (message.notification != null) {
+          print(
+              'Message also contained a notification: ${message.notification}');
+        }
+      });
 
       // For testing purposes print the Firebase Messaging token
       final token = await _firebaseMessaging.getToken();
